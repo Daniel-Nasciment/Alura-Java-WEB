00076574eaf9763eb47a7bef0a7e5e8590e70d95
Daniel <daniel.fecaf@outlook.com>
2022-01-26 17:57:01 -0300

    Recuperando parametros da requisição

---
+++ src/main/java/br/com/alura/servlet/NovaEmpresaServlet.java
@@ -0,0 +1,34 @@
+package br.com.alura.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/novaEmpresa")
+public class NovaEmpresaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+		System.out.println("Cadastrando nova empresa...");
+	
+		PrintWriter writer = response.getWriter();
+		
+		String parameter = request.getParameter("nome");
+		
+		writer.println("<html>");
+		writer.println("<body>");
+		writer.println("<h1>Empresa " + parameter + " cadastrada!!</h1>");
+		writer.println("</body>");
+		writer.println("</html>");
+		
+	}
+
+}
