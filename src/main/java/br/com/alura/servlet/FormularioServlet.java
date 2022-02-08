4f9d4d9699789d9c7a2a83e55441a71ba339892f
Daniel-Nasciment <daniel.fecaf@outlook.com>
2022-02-07 22:35:28 -0300

    Implementando lógica para se autenticar antes de acessar alguns endpoints

--- src/main/java/br/com/alura/servlet/FormularioServlet.java
+++ src/main/java/br/com/alura/servlet/FormularioServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/formularioServlet")
 public class FormularioServlet extends HttpServlet {
@@ -15,6 +16,13 @@ public class FormularioServlet extends HttpServlet {
        
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		HttpSession session = request.getSession();
+		
+		if(session.getAttribute("usuarioLogado") == null) {
+			response.sendRedirect("loginForm");
+			return;
+		}
 		
 		RequestDispatcher requestDispatcher = request.getRequestDispatcher("WEB-INF/view/formNovaEmpresa.jsp");
 		
--- src/main/java/br/com/alura/servlet/ListaEmpresas.java
+++ src/main/java/br/com/alura/servlet/ListaEmpresas.java
@@ -2,6 +2,7 @@ package br.com.alura.servlet;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,6 +10,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import br.com.alura.modelo.Banco;
 import br.com.alura.modelo.Empresa;
@@ -23,13 +25,20 @@ public class ListaEmpresas extends HttpServlet {
 		Banco nubank = new Banco();
 
 		List<Empresa> listaEmpresas = nubank.listaEmpresas();
-			
+
+		HttpSession session = request.getSession();
+
+		if (session.getAttribute("usuarioLogado") == null) {
+			response.sendRedirect("loginForm");
+			return;
+		}
+
 		RequestDispatcher requestDispatcher = request.getRequestDispatcher("WEB-INF/view/listaEmpresas.jsp");
-		
+
 		request.setAttribute("lista", listaEmpresas);
-		
+
 		requestDispatcher.forward(request, response);
-		
+
 	}
 
 }
